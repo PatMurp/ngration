2c1628ff73207741752cfe1873f49ad07807ec47
Patrick Murphy <murphypatv@gmail.com>
2016-02-16 15:24:20 +0000

    seperate existing home page tests into two seperate tests

--- test/app-spec.js
+++ test/app-spec.js
@@ -7,12 +7,19 @@ var app = rewire('../app');
 
 describe("Ngration App", function() {
 
-	it("Loads the home page and checks heading", function(done) {
+	it("Loads the home page", function(done) {
 		request(app).get("/").expect(200).end(function(err, res) {
+			done();
+		});
+	});
+
+	it("Checks H1 heading", function() {
+		request(app).get("/").end(function(err, res) {
 			var $ = cheerio.load(res.text); // load response text
 			var pageHeading = $("body>h1:first-child").text(); // check page heading
 			expect(pageHeading).to.equal("Welcome to Ngration");
 			done();
 		});
 	});
+
 });
