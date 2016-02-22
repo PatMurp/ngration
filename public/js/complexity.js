2558bf0ce5264b8a29fc0a1e7b128633b7b61605
Patrick Murphy <murphypatv@gmail.com>
2016-02-22 16:28:27 +0000

    configure Cyclomatic Complexity max value to 3 & add funcion that will fail

---
+++ public/js/complexity.js
@@ -0,0 +1,25 @@
+"use strict";
+// unused function with a cyclomatic complexity of 4
+// jsHint fails due to .jshintrc maxcomplexity set to 3
+(function(undefined) {
+
+	function isPrime(number) {
+		var prime = true, i;
+
+		if(isNumber(number)) {
+			for (i=2; i< number; i++) {
+				if (number % i === 0) {
+					prime = false;
+				}
+			}
+		} else {
+			prime = false;
+		}
+		return prime;
+	}
+
+	function isNumber(number) {
+		return !isNaN(parseFloat(number)) && isFinite(number);
+	}
+
+}());
