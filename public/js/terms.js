c8ca8b3038dd7707c85651883643fdd2024847bc
Patrick Murphy <murphypatv@gmail.com>
2016-03-24 15:55:45 +0000

    refactor terms.js to conform with code standards

--- public/js/terms.js
+++ public/js/terms.js
@@ -1,28 +1,26 @@
-/* global $:false */
+/* eslint-env jquery */
+/* eslint prefer-template: 0, no-use-before-define: 0, func-names: 0*/
 'use strict';
-$(function () {
-
+$(() => {
   $.getJSON('/dictionary-api', printTerms);
-  $('form').submit(function (e) {
-      e.preventDefault();
-      $.post('/dictionary-api', { term: $('#term').val(), defined: $('#defined').val() }, printTerms);
-      console.log(printTerms);
-      this.reset();
-    });
-
+  $('form').submit((e) => {
+    e.preventDefault();
+    $.post('/dictionary-api', { term: $('#term').val(), defined: $('#defined').val() }, printTerms);
+    $('form').trigger('reset');
+  });
 });
 
 function printTerms(terms) {
   $('body>dl').empty();
   $.each(terms, function () {
-      $('<dt>').text(this.term).appendTo('body>dl');
-      $('<dd>').text(this.defined).appendTo('body>dl');
-    });
+    $('<dt>').text(this.term).appendTo('body>dl');
+    $('<dd>').text(this.defined).appendTo('body>dl');
+  });
   $('dt').off('dblclick').dblclick(function () {
-      $.ajax({
-          url: '/dictionary-api/' + $(this).text(),
-          type: 'DELETE',
-          success: printTerms
-        });
+    $.ajax({
+      url: '/dictionary-api/' + $(this).text(),
+      type: 'DELETE',
+      success: printTerms,
     });
+  });
 }
