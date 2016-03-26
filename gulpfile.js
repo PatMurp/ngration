0ad8e4792600d0937777fbf6976fbe6dd10eedc6
Patrick Murphy <murphypatv@gmail.com>
2016-03-26 14:29:09 +0000

    refactor gulpfile.js to conform to airbnb code standards

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,57 @@
 'use strict';
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
-var mocha = require('gulp-mocha');
-var complexity = require('gulp-complexity');
-var jsinspect = require('gulp-jsinspect');
-var eslint = require('gulp-eslint');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+const mocha = require('gulp-mocha');
+const complexity = require('gulp-complexity');
+const jsinspect = require('gulp-jsinspect');
+const eslint = require('gulp-eslint');
 
-var jsSources,
-		  unitTests;
-
-jsSources = [
-	  'app.js',
-	  'routes.js',
-	  'config/*.js',
-	  'public/js/*js',
-	  'gulpfile.js'
+const jsSources = [
+  'app.js',
+  'routes.js',
+  'config/*.js',
+  'public/js/*js',
+  'gulpfile.js',
 ];
 
-unitTests = [
-	  'test/app-spec.js'
+const unitTests = [
+  'test/app-spec.js',
 ];
 
 // run mocha unit tests
 // !!bug!! mocha tests not exiting when finished
-gulp.task('utest', function () {
-	  return gulp.src(unitTests, { read: false })
-	.pipe(mocha());
-});
+gulp.task('utest', () =>
+    gulp.src(unitTests, { read: false })
+  .pipe(mocha()));
 
 // check for errors in code
-gulp.task('lint', function () {
-	  return gulp.src(jsSources)
-		.pipe(jshint())
-		.pipe(jshint.reporter(stylish))
-		.pipe(jshint.reporter('fail'));
-});
+gulp.task('lint', () =>
+    gulp.src(jsSources)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail')));
 
 // enforce airbnb code standards
-gulp.task('eslint', function () {
-	return gulp.src(jsSources)
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-});
+gulp.task('eslint', () =>
+  gulp.src(jsSources)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()));
 
 // analyse code complexity
-gulp.task('complex', function () {
-	  return gulp.src(jsSources)
-	.pipe(complexity());
-});
+gulp.task('complex', () =>
+    gulp.src(jsSources)
+  .pipe(complexity()));
 
 // detect copy/pasted & structurally similar code
-gulp.task('inspect', function () {
-	  return gulp.src(jsSources)
-	.pipe(jsinspect({
-		  'threshold': 10,
-		  'identifiers': true,
-		  'supress': 0
-	}));
-});
+gulp.task('inspect', () =>
+  gulp.src(jsSources)
+  .pipe(jsinspect({
+    threshold: 10,
+    identifiers: true,
+    supress: 0,
+  })));
 
 gulp.task('default', ['lint']);
 
