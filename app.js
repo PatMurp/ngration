9428641436e63330a665bab1852d30db73d1aac3
Patrick Murphy <murphypatv@gmail.com>
2016-03-26 11:33:30 +0000

    refactor app.js to conform to airbnb code standards

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 'use strict';
-var express = require('express');
+const express = require('express');
 
-var app = express();
+const app = express();
 
 require('./config/express').addMiddleware(app); // add middleware
 require('./routes')(app); // add routes
 
-var skierTerms = require('./public/json/skiterms');
-
+let skierTerms = require('./public/json/skiterms');
 
 // get terms
-app.get('/dictionary-api', function (req, res) {
-	      res.json(skierTerms);
+app.get('/dictionary-api', (req, res) => {
+  res.json(skierTerms);
 });
 
 // add new term
-app.post('/dictionary-api', function (req, res) {
-	      skierTerms.push(req.body);
-	      res.json(skierTerms);
+app.post('/dictionary-api', (req, res) => {
+  skierTerms.push(req.body);
+  res.json(skierTerms);
 });
-
 
 // delete a term
-app.delete('/dictionary-api/:term', function (req, res) {
-	      skierTerms = skierTerms.filter(function (definition) {
-		      return definition.term.toLowerCase() !== req.params.term.toLowerCase();
-	});
-	      res.json(skierTerms);
+app.delete('/dictionary-api/:term', (req, res) => {
+  skierTerms = skierTerms.filter((definition) =>
+     definition.term.toLowerCase() !== req.params.term.toLowerCase()
+  );
+  res.json(skierTerms);
 });
 
-app.listen(process.env.PORT || 4000, function () {
-	      console.log('Express server running on port 4000');
-});
+app.listen(process.env.PORT || 4000);
 
 // export to enable supertest tests
 module.exports = app;
